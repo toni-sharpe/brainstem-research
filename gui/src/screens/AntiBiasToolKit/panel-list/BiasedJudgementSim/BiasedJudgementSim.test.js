e334dde9575cefb6c143c585feb878d492610076
Toni Sharpe <toni.sharpe@nearform.com>
2024-01-05 14:56:41 +0000

    fix 21 canvas blocking test runs
    * added a mock to setupTests file to cover the canvas
    * unskipped the tests
    * removed unneeded stuff and edited package.json
    
    Fixes: https://github.com/toni-sharpe/brainstem-research/issues/21

--- gui/src/screens/AntiBiasToolKit/panel-list/BiasedJudgementSim/BiasedJudgementSim.test.js
+++ gui/src/screens/AntiBiasToolKit/panel-list/BiasedJudgementSim/BiasedJudgementSim.test.js
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom'
-import 'jest-canvas-mock'
 import userEvent from '@testing-library/user-event'
 import { getByLabelText, render, screen, waitFor } from '@testing-library/react'
 
@@ -7,7 +6,7 @@ import APIAntiBiasToolKitData from 'example-data/APIAntiBiasToolKit.example-data
 
 import BiasedJudgementSim from './BiasedJudgementSim'
 
-test.skip('BiasedJudgementSim - ', async () => {
+test('BiasedJudgementSim - ', async () => {
   render(
     <BiasedJudgementSim
       antiBiasToolKitData={APIAntiBiasToolKitData.biased_test_cases}
