ec7871b9c2c4f30304579031c3a675f55c943c19
Toni Sharpe <toni.sharpe@nearform.com>
2024-01-16 01:55:46 +0100

    feat 106 tidied this label too

--- gui/src/components/HistogramBarListLabel/HistogramBarListLabel.stories.js
+++ gui/src/components/HistogramBarListLabel/HistogramBarListLabel.stories.js
@@ -10,7 +10,7 @@ const baseHistogramBarListLabelProps = {
   allBlocks: PRIME_SYMPTOM_HISTOGRAM_BAR_LIST_MAP,
   barCountPerBlock: 3,
   barMargin: 8,
-  blockSize: 64,
+  blockSize: 6,
   i18nKeyOnly: false,
   i18nKey: 'sb-example',
   label: '50..100',
@@ -20,34 +20,6 @@ export const Primary = {
   render: () => {
     return (
       <HistogramBarListLabel {...baseHistogramBarListLabelProps} />
-    )
-  }
-};
-
-export const Smaller = {
-  render: () => {
-    const props = {
-      ...baseHistogramBarListLabelProps,
-      barCountPerBlock: 16,
-      blockSize: 8,
-      label: 50,
-    }
-
-    return (
-      <HistogramBarListLabel {...props} />
-    )
-  }
-};
-
-export const SkipI18n = {
-  render: () => {
-    const props = {
-      ...baseHistogramBarListLabelProps,
-      i18nKeyOnly: true
-    }
-
-    return (
-      <HistogramBarListLabel {...props} />
     )
   }
 };
