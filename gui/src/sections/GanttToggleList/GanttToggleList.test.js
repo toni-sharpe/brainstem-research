3f21cbc507d6917eee386a603d5b6701175b4d95
Toni Sharpe <toni.sharpe@nearform.com>
2024-01-29 07:29:41 +0100

    fix 43 add some click tests to the GanttToggleList

--- gui/src/sections/GanttToggleList/GanttToggleList.test.js
+++ gui/src/sections/GanttToggleList/GanttToggleList.test.js
@@ -6,12 +6,23 @@ import { GANTT_TOGGLE_LIST } from 'util/Constant/BaseConstantList'
 
 import GanttToggleList from './GanttToggleList'
 
-
 const user = userEvent.setup()
 
+const setGanttToggleListStub = jest.fn()
+
 const baseGanttToggleListProps = {
-  setGanttTogglelList: () => {},
-  timeLineBarDetailToggleList: GANTT_TOGGLE_LIST,
+  setGanttToggleList: setGanttToggleListStub,
+  ganttToggleList: {
+    deviation: false,
+    label: false,
+    statList: false,
+    quantile: false,
+    quantileNumber: false,
+    mean: false,
+    median: false,
+    range: false,
+    fatLines: false,
+  },
 }
 
 test('GanttToggleList', async () => {
@@ -25,9 +36,40 @@ test('GanttToggleList', async () => {
   expect(screen.getByText('Label')).toBeTruthy()
   expect(screen.getByText('Stats')).toBeTruthy()
   expect(screen.getByText('Quantile')).toBeTruthy()
-  expect(screen.getByText('Qu\' Numbers')).toBeTruthy()
+  const quNums = screen.getByText('Qu\' Numbers')
+  expect(quNums).toBeTruthy()
   expect(screen.getByText('Mean')).toBeTruthy()
   expect(screen.getByText('Median')).toBeTruthy()
   expect(screen.getByText('Range')).toBeTruthy()
-  expect(screen.getByText('Fat lines')).toBeTruthy()
+  const fatLines = screen.getByText('Fat lines')
+  expect(fatLines).toBeTruthy()
+
+  await user.click(quNums)
+  await user.click(fatLines)
+
+  expect(setGanttToggleListStub.mock.calls.length).toEqual(2)
+
+  expect(setGanttToggleListStub).toHaveBeenCalledWith({
+    deviation: false,
+    fatLines: false,
+    label: false,
+    mean: false,
+    median: false,
+    quantile: false,
+    quantileNumber: true,
+    range: false,
+    statList: false,
+  })
+
+  expect(setGanttToggleListStub).toHaveBeenCalledWith({
+    deviation: false,
+    fatLines: true,
+    label: false,
+    mean: false,
+    median: false,
+    quantile: false,
+    quantileNumber: false,
+    range: false,
+    statList: false,
+  })
 })
