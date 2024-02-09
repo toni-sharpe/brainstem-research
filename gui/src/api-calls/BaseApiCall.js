b28e510395622111eff78ec94dc062d1d77f734d
Toni Sharpe <toni.sharpe@nearform.com>
2024-02-09 21:01:09 +0100

    garden: fix real data
    * Scatter data is actually just the full data object
    * Toggle the API calls more easily

--- gui/src/api-calls/BaseApiCall.js
+++ gui/src/api-calls/BaseApiCall.js
@@ -1,9 +1,11 @@
-// import { API_DOMAIN } from 'util/Constant/SiteConstantList'
+import { API_DOMAIN } from 'util/Constant/SiteConstantList'
 
 import RealAntiBiasJudgementSimData from 'real-data/RealAntiBiasJudgementSim.data'
 import RealFullData from 'real-data/RealFull.data'
 import RealPrimeSymptomListData from 'real-data/RealPrimeSymptomList.data'
 import RealScatterData from 'real-data/RealScatter.data'
+
+const USE_API = false
 
 function baseApiCall ({
   endPoint,
@@ -11,22 +13,24 @@ function baseApiCall ({
 }) {
   return () => {
     async function getData() {
-      // const data = await fetch(`${API_DOMAIN}${endPoint}`)
-      // const dataJson = await data.json()
-      // setData(dataJson)
-
-      if (endPoint === 'PrimeSymptomList') {
-        setData(RealPrimeSymptomListData)
-      }
-      else if (endPoint === 'AntiBiasToolKit') {
-        setData({
-          biased_test_cases: RealAntiBiasJudgementSimData,
-          prime_symptom_cases: RealPrimeSymptomListData,
-          scatter_cases: RealScatterData,
-        })
-      }
-      else {
-        setData(RealFullData)
+      if (USE_API) {
+        const data = await fetch(`${API_DOMAIN}${endPoint}`)
+        const dataJson = await data.json()
+        setData(dataJson)
+      } else {
+        if (endPoint === 'PrimeSymptomList') {
+          setData(RealPrimeSymptomListData)
+        }
+        else if (endPoint === 'AntiBiasToolKit') {
+          setData({
+            biased_test_cases: RealAntiBiasJudgementSimData,
+            prime_symptom_cases: RealPrimeSymptomListData,
+            scatter_cases: RealScatterData,
+          })
+        }
+        else {
+          setData(RealFullData)
+        }
       }
     }
     getData()
