1f3293a919a913549c6878f0be6c967c839caacd
Toni Sharpe <toni.sharpe@nearform.com>
2024-02-29 00:47:16 +0100

    spike 335 large number of Drag labels
    * responding to something raised by OWID I wanted to have a look at this
    * an initial spike is a bit crazy but possibly there is potential

--- gui/src/sections/DragGraph/DragGraph.stories.js
+++ gui/src/sections/DragGraph/DragGraph.stories.js
@@ -11,7 +11,6 @@ export default {
 const divWrapper = { boxShadow: '0 0 100px 0 #000', height: '690px', width: '540px' }
 
 const baseDragGraphProps = {
-  color: 'green',
   dragGraphZoomList: [1, 2, 3, 4],
   graphKey: 'dragGraphStoryBook',
   heading: 'Graph Heading',
@@ -122,7 +121,6 @@ export const TwoPoints = {
   render: () => {
     const props = {
       ...baseDragGraphProps,
-      color: 'red',
       includeExtreme: true,
       labelValList: [
         ['a', { careLevel:  13, length: 7 , nonSevere:  3, severe:  4 }],
@@ -141,7 +139,6 @@ export const ThreePoints = {
   render: () => {
     const props = {
       ...baseDragGraphProps,
-      color: '#9900cc',
       includeExtreme: true,
       labelValList: [
         ['a', { careLevel:  23, length: 7 , nonSevere:  2, severe:  5 }],
@@ -161,7 +158,6 @@ export const FourPoints = {
   render: () => {
     const props = {
       ...baseDragGraphProps,
-      color: 'red',
       includeExtreme: true,
       labelValList: [
         ['a', { careLevel:  45, length: 7 , nonSevere:  3, severe:  4 }],
@@ -182,7 +178,6 @@ export const FivePoints = {
   render: () => {
     const props = {
       ...baseDragGraphProps,
-      color: 'blue',
       includeExtreme: true,
       labelValList: [
         ['a', { careLevel:  12, length: 19, nonSevere:  3, severe: 13 }],
@@ -204,7 +199,6 @@ export const LotsOfPoints = {
   render: () => {
     const props = {
       ...baseDragGraphProps,
-      color: '#0055bb',
       includeExtreme: true,
       labelValList: [
         ['a', { careLevel:  12, length: 19, nonSevere: 16, severe:  3 }],
@@ -243,7 +237,6 @@ export const BigRange = {
   render: () => {
     const props = {
       ...baseDragGraphProps,
-      color: '#333',
       includeExtreme: true,
       labelValList: [
         ['a', { careLevel:   1, length: 19,   nonSevere:  3, severe: 10 }],
@@ -299,3 +292,151 @@ export const NotEnoughData = {
     )
   }
 };
+
+export const LayersOfPoints = {
+  render: () => {
+    const props = {
+      ...baseDragGraphProps,
+      includeExtreme: true,
+      useDepth: true,
+      scale: 1200,
+      scaleR: 600,
+      labelValList:
+        [
+          ['34a',    { careLevel:  34, length: 19, nonSevere: 16, severe:  3 }],
+          ['19b',    { careLevel:  19, length: 11, nonSevere: 20, severe:  3 }],
+          ['14c',    { careLevel:  14, length: 12, nonSevere: 40, severe:  1 }],
+          ['23d',    { careLevel:  23, length: 51, nonSevere: 11, severe: 10 }],
+          ['16e',    { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['41f',    { careLevel:  41, length:  5, nonSevere:  3, severe:  2 }],
+          ['98g',    { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['31h',    { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['102i',   { careLevel: 102, length: 23, nonSevere: 15, severe:  8 }],
+          ['29j',    { careLevel:  29, length: 27, nonSevere: 11, severe: 12 }],
+          ['41k',    { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['31l',    { careLevel:  31, length: 41, nonSevere: 40, severe:  7 }],
+          ['29m',    { careLevel:  29, length: 11, nonSevere:  7, severe:  4 }],
+          ['39n',    { careLevel:  39, length: 19, nonSevere:  7, severe: 12 }],
+          ['51o',    { careLevel:  51, length: 11, nonSevere:  8, severe:  3 }],
+          ['33p',    { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['12aa',   { careLevel:  12, length: 19, nonSevere: 16, severe:  3 }],
+          ['19bb',   { careLevel:  19, length: 23, nonSevere: 20, severe:  3 }],
+          ['14cc',   { careLevel:  14, length: 41, nonSevere: 40, severe:  1 }],
+          ['29dd',   { careLevel:  29, length: 31, nonSevere: 11, severe: 10 }],
+          ['16ee',   { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['41ff',   { careLevel:  41, length:  5, nonSevere:  3, severe:  2 }],
+          ['98gg',   { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['31hh',   { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['102ii',  { careLevel: 102, length: 23, nonSevere: 15, severe:  8 }],
+          ['29jj',   { careLevel:  29, length: 27, nonSevere: 15, severe: 12 }],
+          ['41kk',   { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['31ll',   { careLevel:  31, length: 41, nonSevere: 34, severe:  7 }],
+          ['29mm',   { careLevel:  29, length: 11, nonSevere:  7, severe:  4 }],
+          ['39nn',   { careLevel:  39, length: 19, nonSevere:  7, severe: 98 }],
+          ['51oo',   { careLevel:  51, length: 11, nonSevere:  8, severe: 11 }],
+          ['33pp',   { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['23aaa',  { careLevel:  23, length: 19, nonSevere: 16, severe:  3 }],
+          ['20bbb',  { careLevel:  20, length: 23, nonSevere: 20, severe:  3 }],
+          ['14ccc',  { careLevel:  14, length: 41, nonSevere: 40, severe:  1 }],
+          ['10ddd',  { careLevel:  10, length: 31, nonSevere: 11, severe: 10 }],
+          ['16eee',  { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['41fff',  { careLevel:  41, length:  5, nonSevere:  3, severe:  2 }],
+          ['98ggg',  { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['31hhh',  { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['102iii', { careLevel: 102, length: 23, nonSevere: 15, severe:  8 }],
+          ['29jjj',  { careLevel:  29, length: 27, nonSevere: 15, severe: 12 }],
+          ['41kkk',  { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['31lll',  { careLevel:  31, length: 41, nonSevere: 34, severe:  7 }],
+          ['29mmm',  { careLevel:  29, length: 16, nonSevere:  7, severe:  4 }],
+          ['39nnn',  { careLevel:  39, length: 19, nonSevere:  7, severe: 12 }],
+          ['51ooo',  { careLevel:  51, length: 11, nonSevere:  8, severe:  3 }],
+          ['33ppp',  { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['12aaa4', { careLevel:  12, length: 29, nonSevere: 16, severe:  3 }],
+          ['19bbb4', { careLevel:  19, length: 23, nonSevere: 20, severe:  3 }],
+          ['14ccc4', { careLevel:  14, length: 41, nonSevere: 40, severe:  1 }],
+          ['23ddd4', { careLevel:  23, length: 31, nonSevere: 11, severe: 10 }],
+          ['16eee4', { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['3fff4',  { careLevel:   3, length:  5, nonSevere:  3, severe:  2 }],
+          ['98ggg4', { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['31lll4', { careLevel:  31, length: 41, nonSevere: 34, severe:  7 }],
+          ['237aa',  { careLevel:  23, length: 19, nonSevere: 16, severe:  3 }],
+          ['197',    { careLevel:  19, length: 11, nonSevere: 20, severe:  3 }],
+          ['11mmm4', { careLevel:  11, length: 51, nonSevere:  7, severe:  4 }],
+          ['417k',   { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['33ppp4', { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['167e',   { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['237',    { careLevel:  23, length: 51, nonSevere: 11, severe: 10 }],
+          ['517o',   { careLevel:  51, length: 11, nonSevere:  8, severe: 11 }],
+          ['417f',   { careLevel:  41, length:  5, nonSevere:  3, severe:  2 }],
+          ['297j',   { careLevel:  29, length: 27, nonSevere: 15, severe: 12 }],
+          ['317l',   { careLevel:  31, length: 41, nonSevere: 34, severe:  7 }],
+          ['29iii4', { careLevel:  29, length: 23, nonSevere: 15, severe:  8 }],
+          ['397n',   { careLevel:  39, length: 19, nonSevere:  7, severe: 98 }],
+          ['337p',   { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['987g',   { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['39nnn4', { careLevel:  39, length: 19, nonSevere:  7, severe: 12 }],
+          ['147',    { careLevel:  14, length: 12, nonSevere: 40, severe:  1 }],
+          ['41kkk4', { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['29jjj4', { careLevel:  29, length: 27, nonSevere: 15, severe: 12 }],
+          ['417',    { careLevel:  41, length:  5, nonSevere:  3, severe:  2 }],
+          ['167',    { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['317h',   { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['31hhh4', { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['1027i',  { careLevel: 102, length: 23, nonSevere: 15, severe:  8 }],
+          ['51ooo4', { careLevel:  51, length: 11, nonSevere:  8, severe:  3 }],
+          ['297m',   { careLevel:  29, length: 11, nonSevere:  7, severe:  4 }],
+          ['297d',   { careLevel:  29, length: 31, nonSevere: 11, severe: 10 }],
+          ['147c',   { careLevel:  14, length: 41, nonSevere: 40, severe:  1 }],
+          ['197b',   { careLevel:  19, length: 23, nonSevere: 20, severe:  3 }],
+          ['127a',   { careLevel:  12, length: 19, nonSevere: 16, severe:  3 }],
+          ['337',    { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['517',    { careLevel:  51, length: 11, nonSevere:  8, severe:  3 }],
+          ['397',    { careLevel:  39, length: 19, nonSevere:  7, severe: 12 }],
+          ['297',    { careLevel:  29, length: 11, nonSevere:  7, severe:  4 }],
+          ['317',    { careLevel:  31, length: 41, nonSevere: 40, severe:  7 }],
+          ['417',    { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['297',    { careLevel:  29, length: 27, nonSevere: 11, severe: 12 }],
+          ['1027',   { careLevel: 102, length: 23, nonSevere: 15, severe:  8 }],
+          ['317',    { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['987',    { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['207bb',  { careLevel:  20, length: 23, nonSevere: 20, severe:  3 }],
+          ['147cc',  { careLevel:  14, length: 41, nonSevere: 40, severe:  1 }],
+          ['107dd',  { careLevel:  10, length: 31, nonSevere: 11, severe: 10 }],
+          ['167ee',  { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['417ff',  { careLevel:  41, length:  5, nonSevere:  3, severe:  2 }],
+          ['987gg',  { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['317hh',  { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['1027ii', { careLevel: 102, length: 23, nonSevere: 15, severe:  8 }],
+          ['297jj',  { careLevel:  29, length: 27, nonSevere: 15, severe: 12 }],
+          ['417kk',  { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['317ll',  { careLevel:  31, length: 41, nonSevere: 34, severe:  7 }],
+          ['297mm',  { careLevel:  29, length: 16, nonSevere:  7, severe:  4 }],
+          ['397nn',  { careLevel:  39, length: 19, nonSevere:  7, severe: 12 }],
+          ['517oo',  { careLevel:  51, length: 11, nonSevere:  8, severe:  3 }],
+          ['337pp',  { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+          ['127aa4', { careLevel:  12, length: 29, nonSevere: 16, severe:  3 }],
+          ['197bb4', { careLevel:  19, length: 23, nonSevere: 20, severe:  3 }],
+          ['147cc4', { careLevel:  14, length: 41, nonSevere: 40, severe:  1 }],
+          ['237dd4', { careLevel:  23, length: 31, nonSevere: 11, severe: 10 }],
+          ['167ee4', { careLevel:  16, length: 29, nonSevere: 10, severe: 19 }],
+          ['37ff4',  { careLevel:   3, length:  5, nonSevere:  3, severe:  2 }],
+          ['987gg4', { careLevel:  98, length: 17, nonSevere: 12, severe:  5 }],
+          ['317hh4', { careLevel:  31, length:  7, nonSevere:  3, severe:  4 }],
+          ['297ii4', { careLevel:  29, length: 23, nonSevere: 15, severe:  8 }],
+          ['297jj4', { careLevel:  29, length: 27, nonSevere: 15, severe: 12 }],
+          ['417kk4', { careLevel:  41, length: 23, nonSevere: 15, severe:  8 }],
+          ['317ll4', { careLevel:  31, length: 41, nonSevere: 34, severe:  7 }],
+          ['117mm4', { careLevel:  11, length: 51, nonSevere:  7, severe:  4 }],
+          ['397nn4', { careLevel:  39, length: 19, nonSevere:  7, severe: 12 }],
+          ['517oo4', { careLevel:  51, length: 11, nonSevere:  8, severe:  3 }],
+          ['337pp4', { careLevel:  33, length: 39, nonSevere: 30, severe:  9 }],
+        ]
+      }
+    return (
+      <div style={{...{ boxShadow: '0 0 100px 0 #000', height: '1200px', width: '1200px' }}}>
+        <DragGraph {...props} />
+      </div>
+    )
+  }
+};
+
