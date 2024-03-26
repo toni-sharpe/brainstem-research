bf39dbfbb4e0c1548ef1339724434ce91abd2626
Toni Sharpe <toni.sharpe@nearform.com>
2024-03-26 15:52:42 +0100

    fix tests broken in the last commit

--- gui/src/util/UtilWorldMapZoomMark/UtilWorldMapVerticalZoomMark.test.js
+++ gui/src/util/UtilWorldMapZoomMark/UtilWorldMapVerticalZoomMark.test.js
@@ -5,33 +5,33 @@ import {
 
 test('calcVerticalZoomMarkHeight() should return correct height when zoom is 2', () => {
   const zoom = 2
-  expect(calcVerticalZoomMarkHeight({ zoom })).toBe('39.2')
+  expect(calcVerticalZoomMarkHeight({ zoom })).toBe('38.5')
 })
 
 test('calcVerticalZoomMarkHeight() should return correct height when zoom is 1', () => {
   const zoom = 1
-  expect(calcVerticalZoomMarkHeight({ zoom })).toBe('78.4')
+  expect(calcVerticalZoomMarkHeight({ zoom })).toBe('77.0')
 })
 
 test('calcVerticalZoomMarkHeight() should return correct height when zoom is 0.5', () => {
   const zoom = 0.5
-  expect(calcVerticalZoomMarkHeight({ zoom })).toBe('156.8')
+  expect(calcVerticalZoomMarkHeight({ zoom })).toBe('154.0')
 })
 
 test('should return correct top value when y is positive and zoom is 2', () => {
   const y = 20
   const zoom = 2
-  expect(calcVerticalZoomMarkTop({ y, zoom })).toBe(1.64017)
+  expect(calcVerticalZoomMarkTop({ y, zoom })).toBe(1.61088)
 })
 
 test('should return correct top value when y is negative and zoom is 1', () => {
   const y = -30
   const zoom = 1
-  expect(calcVerticalZoomMarkTop({ y, zoom })).toBe(4.9205)
+  expect(calcVerticalZoomMarkTop({ y, zoom })).toBe(4.83264)
 })
 
 test('should return correct top value when y is positive and zoom is 0.5', () => {
   const y = 40
   const zoom = 0.5
-  expect(calcVerticalZoomMarkTop({ y, zoom })).toBe(13.1213)
+  expect(calcVerticalZoomMarkTop({ y, zoom })).toBe(12.887)
 })
